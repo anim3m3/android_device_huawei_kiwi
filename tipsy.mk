26479ef3c192e9f82853281f0a847d9d085e2725
Asanan Aphisitworachorch <thaixperiasp@gmail.com>
2017-02-13 18:40:20 +0700

    Tipsy : Be Tipsy !
    Change-Id: I8d7858b788d19d22cf1309aaf03c835a87354d1b
    Signed-off-by: Asanan Aphisitworachorch <thaixperiasp@gmail.com>

--- tipsy.mk
+++ tipsy.mk
@@ -15,9 +15,9 @@
 $(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_kiwi
+PRODUCT_NAME := tipsy_kiwi
 BOARD_VENDOR := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
